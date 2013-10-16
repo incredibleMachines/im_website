3ed4e2ab1cb9b5d706af3dcd08ab06015bbdec34
Jennifer Kaye <jenniferkaye7@gmail.com>
2013-10-16 17:36:31 -0400

    connected all form data for single project page

--- functions/authenticate.js
+++ functions/authenticate.js
@@ -3,6 +3,7 @@ exports.admin = function(req, res, next){
 		//check that date and key match up
 		console.log('page-access-denied');
 		//console.log(req);
+		console.log(req.path);
 		res.render('admin-login', {title:'You must login to access.', slug:'admin-login', path:req.path});
 
 	}else{
@@ -22,18 +23,23 @@ exports.project = function(db){
 
 		projects.findOne({slug:name}, 'password',function(err,doc){
 			if(err) throw err;
-			if(!doc) next(); //must ensure we call next to continue processes
-			if(doc.password===null) next();
+			if(!doc) next();  //must ensure we call next to continue processes
+			//console.log(doc.password)
 
+			if(doc.password===null){ next();
+			}else if(doc.password == null){ next();
 			//we have a password situation here..
-			if(!req.session.project){
-				//console.log(req);
-				res.render('project-login',{title:'This Project Requires Login', slug: 'project-login', project_slug: name });
-				console.log(doc.password);
 			}else{
-				if(req.session.project === name) next();	
-				else res.render('project-login',{title:'This Project Requires Login', slug: 'project-login', project_slug: name });
 
+				if(!req.session.project){
+					//console.log(req);
+					res.render('project-login',{title:'This Project Requires Login', slug: 'project-login', project_slug: name });
+					console.log(doc.password);
+				}else{
+					if(req.session.project === name) next();	
+					else res.render('project-login',{title:'This Project Requires Login', slug: 'project-login', project_slug: name });
+
+				}
 			}
 			
 		});
