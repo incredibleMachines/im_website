7b5013ad1bd2217897f9bfaecf5ba813ede8bf38
petermania <scott@incrediblemachines.net>
2013-10-20 14:59:12 -0400

    fixed issues with clients page scroll height and padding

--- public/javascripts/clients.js
+++ public/javascripts/clients.js
@@ -4,7 +4,6 @@ $(document).ready(function(){
 
 	var clientSize=42;
 
-
 		$('.client h2').each(function(){
 			$(this).css('font-size',clientSize);
 			for(var i=0;i<parseInt($(this).css('font-size'));i++){
@@ -28,7 +27,8 @@ $(document).ready(function(){
 			var gridHeight=$('#client-list').height()-2*parseInt($('.cap-tech').css('margin-top'));
 			$("#"+clientSlug).show(600,function(){
 				$('.client').hide();
-				$('.cap-tech').css('padding-top', $('.client-detail').height()+parseInt($('.cap-tech').css('margin-top')));
+				$('.cap-tech').css('padding-top', $("#"+clientSlug).outerHeight()+30);
+				$(window).scrollTop(0);
 			});
 			console.log(gridHeight);
 			;
@@ -43,7 +43,8 @@ $(document).ready(function(){
 			var gridHeight=$('#client-list').height()-2*parseInt($('.cap-tech').css('margin-top'));
 			$("#"+clientSlug).show(600,function(){
 				$('.client').hide();
-				$('.cap-tech').css('padding-top', $('.client-detail').height()+parseInt($('.cap-tech').css('margin-top')));
+				$('.cap-tech').css('padding-top', $("#"+clientSlug).outerHeight()+30);
+				$(window).scrollTop(0);
 			});
 			console.log(gridHeight);
 			;
@@ -54,29 +55,54 @@ $(document).ready(function(){
 		else if (target.parents('.close').length>0){
 			$('.client').fadeIn();
 			$('.client-detail').hide(600);
-			$('.cap-tech').css('padding-top',0);
+			$('.cap-tech').css('padding-top',30);
 			
 			$('#client-grid h1').fadeOut(function(){
 				$('#client-grid h1').text("Clients").fadeIn();
 			});
+
+			$('.client h2').each(function(){
+			$(this).css('font-size',clientSize);
+			for(var i=0;i<parseInt($(this).css('font-size'));i++){
+				if($(this).innerHeight()>clientSize+parseInt($(this).css('padding-top'))+parseInt($(this).css('padding-bottom'))+10){
+					$(this).css('font-size',parseInt($(this).css('font-size'))-1);
+				}
+				else{
+					break;
+				}
+			}
+		});
 		}
 		else if(!target.is('.client-detail')&&target.parents('.client-detail').length==0){
 			$('.client').fadeIn();
 			$('.client-detail').hide(600);
-			$('.cap-tech').css('padding-top',0);
+			$('.cap-tech').css('padding-top',30);
 			
 			$('#client-grid h1').fadeOut(function(){
 				$('#client-grid h1').text("Clients").fadeIn();
 			});
+					$('.client h2').each(function(){
+			$(this).css('font-size',clientSize);
+			for(var i=0;i<parseInt($(this).css('font-size'));i++){
+				if($(this).innerHeight()>clientSize+parseInt($(this).css('padding-top'))+parseInt($(this).css('padding-bottom'))+10){
+					$(this).css('font-size',parseInt($(this).css('font-size'))-1);
+				}
+				else{
+					break;
+				}
+			}
+		});
 		}
 	});
 	
 
 	userResize= function(){
 
-		if($('.client-detail').css('display')=='block'){
-			$('.cap-tech').css({'padding-top': $('.client-detail').height()+parseInt($('.cap-tech').css('margin-top'))},500);
-		}
+		$('.client-detail').each(function(){
+			if($(this).css('display')=='block'){
+				$('.cap-tech').css('padding-top', $(this).outerHeight()+30);
+			}
+		});
 
 		$('.client h2').each(function(){
 			$(this).css('font-size',clientSize);
