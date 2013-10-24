d9fa77cf2cf2ac0bcfd6c0ac12d4c4948d111316
petermania <scott@incrediblemachines.net>
2013-10-24 14:48:04 -0400

    all scrolling in

--- public/javascripts/projects.js
+++ public/javascripts/projects.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
 
 $('nav a').removeClass('active');
+$('nav a:first-child').attr("href",'#portfolio');
 $('.video-js').css('position','absolute');
 $('.introLogo').find('img').attr('src','../images/im_logo_white.png');
 $('.logoHome').hide();
@@ -203,5 +204,20 @@ function shuffle(array) {
 
   return array;
 }
+
+	$('a[href*=#]:not([href=#])').click(function() {
+	    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+
+	      var target = $(this.hash);
+	      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+	      if (target.length) {
+	        $('html,body').animate({
+	          scrollTop: target.offset().top
+	        }, 500);
+	        return false;
+	      }
+	    }
+	  });
+
 
 });
